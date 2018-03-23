ca4c0c4353ec56288a8b347e72fd98e04af565ff
Patrick J Louis <lumenfuzz@gmail.com>
2018-03-23 11:42:20 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,61 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def self.create
+    song = self.new
+    @@all << song
+    return song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    return song
+  end
+
+  def self.new_from_filename(filename)
+    song = self.new
+    file_data = filename.split(" - ")
+    song.name = file_data[1]
+    ext_index = file_data[0].lastIndexOf('.')
+    file_data[0] = file_data[0].slice(0,ext_index)
+    song.artist_name = file_data[0]
+    return song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    return song
+  end
+
+  def self.find_by_name(name)
+    @@all.each do |song|
+      return song if song.name == name
+    end
+    return nil
+  end
+
+  def self.find_or_create_by_name(name)
+    song = self.find_by_name(name)
+    return song if song != nil
+    return self.create_by_name(name) if song == nil
+  end
+
+  def self.alphabetical
+    name_array = []
+    @@all.each do |song|
+      name_array << song.name
+    end
+    name_array.sort!
+    song_array = []
+    name_array.each do |name|
+      song_array << self.find_by_name(name)
+    end
+    return song_array
+  end
+
   def self.all
     @@all
   end
